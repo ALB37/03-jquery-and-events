5ab5846c670049c312abbc72987a05fe5c292f46
Andrew Bloom <blooma116@gmail.com>
2017-10-26 10:40:48 -0700

    handleAuthorFilter TODOs DONE

--- andrew-rob/scripts/articleView.js
+++ andrew-rob/scripts/articleView.js
@@ -37,17 +37,17 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEWED: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
+    let $articles = $('#articles');
     if ($(this).val()) {
       // DONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
-      let $articles = $('#articles');
       $articles.find('article').hide();
       console.log($(this).val());
       $articles.find(`[data-author='${$(this).val()}']`).fadeIn(1250);
 
     } else {
-      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-
+      // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      $articles.find('article:not(.template)').show();
     }
     $('#category-filter').val('');
   });
