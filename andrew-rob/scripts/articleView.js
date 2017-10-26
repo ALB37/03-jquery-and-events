aa61179055304bed3472cb35d4a7b093b507773b
Robert Reed <robert.mc.reed@gmail.com>
2017-10-26 12:08:06 -0700

    articles showing preview only, reveals all on click

--- andrew-rob/scripts/articleView.js
+++ andrew-rob/scripts/articleView.js
@@ -75,18 +75,23 @@ articleView.handleMainNav = function() {
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
   $( 'main' ).children().hide();
-  $( `#${$( this ).attr( 'data-content' )}` ).show();
+  $( `#${$( this ).attr( 'data-content' )}` ).fadeIn( 1250 );
 };
 
 articleView.setTeasers = function() {
-  // REVIEW: Hide elements beyond the first 2 in any article body.
+  // REVIEWED: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  $( '#articles' ).on( 'click', '.read-on', function( e ) {
+    e.preventDefault();
+    $( this ).prev().children().show();
+    $( this ).hide();
+  });
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
@@ -94,4 +99,5 @@ $(document).ready(function() {
   // REVIEWED: Now trigger a click on the first .tab element, to set up the page.
   $( '#about' ).hide(); //Note: Our approach to this was different, but effective.
   $( '.tab' ).on( 'click', articleView.handleMainNav );
+  articleView.setTeasers();
 })
